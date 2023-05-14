1c3472485f68214bfbfbbaba3b5ee9733ecdbfd3
Zemorath <trentward10@gmail.com>
2023-05-14 15:54:51 -0500

    Array Method Lab Complete

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() { 
+  let cased = tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ');
+  });
+
+  console.log(cased);
+  return cased;
 }
+
+titleCased(tutorials);
